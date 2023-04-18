69fe45a463a6f30f30d69392dabb656ff6f610e3
Olga F <krasota1_00@mail.ru>
2023-04-18 15:23:42 +0700

    README.md

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void checkingStatus() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Netology")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Netology"));
+
+    }
+}
+
